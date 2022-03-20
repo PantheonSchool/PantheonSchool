9ee37ae2d8dd90f9d0f481102e93c6cceebfe90d
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-03-20 23:40:43 +0530

    Nav Data Added

--- src/components/Navbar/navList.js
+++ src/components/Navbar/navList.js
@@ -1,76 +1,87 @@
 const navList = [
-    {
-        title:'ABOUT',
-        hasList:true,
-        list:[
-            {
-                title:'Pantheon at a Glance'
-            },
-            {
-                title:'History of Pantheon'
-            },
-            {
-                title:'Diversity & Inclusion'
-            },
-            {
-                title:'Calendar'
-            },
-            {
-                title:'Leadership'
-            },
-            {
-                title:'Working at Pantheon'
-            },
-            {
-                title:'Explore Pantheon'
-            },
-            {
-                title:'Technology at Pantheon'
-            }
-        ]
-    },
-    {
-        title:'ADMISSION',
-        hasList:true
-    },
-    {
-        title:'ACADEMICS',
-        hasList:true
-    },
-    {
-        title:'ATHLETICS',
-        hasList:true
-    },
-    {
-        title:'ARTS',
-        hasList:true,
-        list:[
-            {
-                title:'Pantheon at a Glance'
-            },
-            {
-                title:'History of Pantheon'
-            },
-            {
-                title:'Diversity & Inclusion'
-            },
-            {
-                title:'Calendar'
-            },
-            {
-                title:'Leadership'
-            },
-            {
-                title:'Working at Pantheon'
-            },
-            {
-                title:'Explore Pantheon'
-            },
-            {
-                title:'Technology at Pantheon'
-            }
-        ]
-    }
-]
+  {
+    title: "ABOUT",
+    hasList: true,
+    list: [
+      {
+        title: "Pantheon at a Glance",
+      },
+      {
+        title: "History of Pantheon",
+      },
+      {
+        title: "Diversity & Inclusion",
+      },
+      {
+        title: "Calendar",
+      },
+      {
+        title: "Leadership",
+      },
+      {
+        title: "Working at Pantheon",
+      },
+      {
+        title: "Explore Pantheon",
+      },
+      {
+        title: "Technology at Pantheon",
+      },
+    ],
+  },
+  {
+    title: "ADMISSION",
+    hasList: true,
+    list: [
+      { title: "Admission Information" },
+      { title: "Fee Structure" },
+      { title: "Apply Online" },
+    ],
+  },
+  {
+    title: "ACADEMICS",
+    hasList: true,
+    list: [
+      { title: "Academic Calendar" },
+      { title: "Assesment Schedule" },
+      { title: "Curriculum" },
+      { title: "Syllabus" },
+    ],
+  },
+  {
+    title: "ATHLETICS",
+    hasList: true,
+  },
+  {
+    title: "ARTS",
+    hasList: true,
+    list: [
+      {
+        title: "Pantheon at a Glance",
+      },
+      {
+        title: "History of Pantheon",
+      },
+      {
+        title: "Diversity & Inclusion",
+      },
+      {
+        title: "Calendar",
+      },
+      {
+        title: "Leadership",
+      },
+      {
+        title: "Working at Pantheon",
+      },
+      {
+        title: "Explore Pantheon",
+      },
+      {
+        title: "Technology at Pantheon",
+      },
+    ],
+  },
+];
 
-export default navList
+export default navList;
