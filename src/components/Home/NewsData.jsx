3f5cbeefe8d72d39ac2717b8e5223109394bdd8b
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-03-22 16:56:12 +0530

    News Data Added

--- src/components/Home/NewsData.jsx
+++ src/components/Home/NewsData.jsx
@@ -1,28 +1,28 @@
 const posts = [
-    {
-        img:'https://assets.speakcdn.com/assets/2532/2nd_gen-8.jpg',
-        date:'Friday, March 11, 2022',
-        title:'PYP Unit Takes First-Graders On A Voyage To Mars',
-        body:"This year's Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, \"Where We Are In Place and Time,\" the annual class project explores how we can create pr..."
-    },
-    {
-        img:'https://assets.speakcdn.com/assets/2532/2nd_gen-8.jpg',
-        date:'Friday, March 11, 2022',
-        title:'PYP Unit Takes First-Graders On A Voyage To Mars',
-        body:"This year's Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, \"Where We Are In Place and Time,\" the annual class project explores how we can create pr..."
-    },
-    {
-        img:'https://assets.speakcdn.com/assets/2532/2nd_gen-8.jpg',
-        date:'Friday, March 11, 2022',
-        title:'PYP Unit Takes First-Graders On A Voyage To Mars',
-        body:"This year's Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, \"Where We Are In Place and Time,\" the annual class project explores how we can create pr..."
-    },
-    {
-        img:'https://assets.speakcdn.com/assets/2532/2nd_gen-8.jpg',
-        date:'Friday, March 11, 2022',
-        title:'PYP Unit Takes First-Graders On A Voyage To Mars',
-        body:"This year's Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, \"Where We Are In Place and Time,\" the annual class project explores how we can create pr..."
-    }
-]
+  {
+    img: "https://scontent.fdel31-1.fna.fbcdn.net/v/t39.30808-6/274857122_948365115838197_3837597614428638180_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=iceijvBIeS4AX9vQ6I9&_nc_ht=scontent.fdel31-1.fna&oh=00_AT_TjH8Cd-Qh7cRRJpNW5b6FdQAM5_NzTyiQwnhHULbkJA&oe=623EEC46",
+    date: "Saturday, February 26, 2022",
+    title: "International Abacus Competition",
+    body: "Panshul Tewari of class 5 and Aman Kumar of class 2 of the School secured position in International Abacus Competition. The Pantheon School congratulates both of them for their achievement.",
+  },
+  {
+    img: "https://scontent.fdel31-1.fna.fbcdn.net/v/t39.30808-6/274687035_946192969388745_2395329250587245072_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=vlox40ZlH94AX8HuNQg&_nc_ht=scontent.fdel31-1.fna&oh=00_AT-nOW4XDy1RJiCxN6_mgYvBfpIKlMIV6-tlohlN-tcmVg&oe=623EC66D",
+    date: "Friday, March 11, 2022",
+    title: "PYP Unit Takes First-Graders On A Voyage To Mars",
+    body: 'This year\'s Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, "Where We Are In Place and Time," the annual class project explores how we can create pr...',
+  },
+  {
+    img: "https://scontent.fdel31-1.fna.fbcdn.net/v/t39.30808-6/274650696_947338165940892_2300708138190429864_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=r3nHSdQ8SWcAX8pdUJL&_nc_ht=scontent.fdel31-1.fna&oh=00_AT9zBq7wf0LL2BjQtQwb8CS4xHJGsoYA3zI_2pmYGgVn5g&oe=623DFFB5",
+    date: "Friday, March 11, 2022",
+    title: "PYP Unit Takes First-Graders On A Voyage To Mars",
+    body: 'This year\'s Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, "Where We Are In Place and Time," the annual class project explores how we can create pr...',
+  },
+  {
+    img: "https://scontent.fdel31-1.fna.fbcdn.net/v/t39.30808-6/274715074_949185742422801_1899635053810478259_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=yfDWr-bgAXUAX_iA0Jg&_nc_ht=scontent.fdel31-1.fna&oh=00_AT8Kw68nJ__pfTvkNtq3U4_VG_hpQysuwyo1uYQLcZzbRQ&oe=623F8CEC",
+    date: "Friday, March 11, 2022",
+    title: "PYP Unit Takes First-Graders On A Voyage To Mars",
+    body: 'This year\'s Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, "Where We Are In Place and Time," the annual class project explores how we can create pr...',
+  },
+];
 
 export default posts;
