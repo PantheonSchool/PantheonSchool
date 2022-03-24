7c3412950db4ea0adc28752abba40fc54fc296e8
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-03-24 22:14:12 +0530

    data added

--- src/components/Home/NewsData.jsx
+++ src/components/Home/NewsData.jsx
@@ -1,4 +1,4 @@
-import Abacus from '../Images/news-pics/Abacus.png'
+import Abacus from "../Images/news-pics/Abacus.png";
 //images should be in 640:427px
 
 const posts = [
@@ -11,20 +11,20 @@ const posts = [
   {
     img: "https://scontent.fdel31-1.fna.fbcdn.net/v/t39.30808-6/274687035_946192969388745_2395329250587245072_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=vlox40ZlH94AX8HuNQg&_nc_ht=scontent.fdel31-1.fna&oh=00_AT-nOW4XDy1RJiCxN6_mgYvBfpIKlMIV6-tlohlN-tcmVg&oe=623EC66D",
     date: "Friday, March 11, 2022",
-    title: "PYP Unit Takes First-Graders On A Voyage To Mars",
-    body: 'This year\'s Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, "Where We Are In Place and Time," the annual class project explores how we can create pr...',
+    title: "Students Enjoying their sports period",
+    body: "",
   },
   {
     img: "https://scontent.fdel31-1.fna.fbcdn.net/v/t39.30808-6/274650696_947338165940892_2300708138190429864_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=r3nHSdQ8SWcAX8pdUJL&_nc_ht=scontent.fdel31-1.fna&oh=00_AT9zBq7wf0LL2BjQtQwb8CS4xHJGsoYA3zI_2pmYGgVn5g&oe=623DFFB5",
-    date: "Friday, March 11, 2022",
-    title: "PYP Unit Takes First-Graders On A Voyage To Mars",
-    body: 'This year\'s Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, "Where We Are In Place and Time," the annual class project explores how we can create pr...',
+    date: "Thursday, February 24, 2022",
+    title: "pre primary students celebrating orange day",
+    body: "",
   },
   {
     img: "https://scontent.fdel31-1.fna.fbcdn.net/v/t39.30808-6/274715074_949185742422801_1899635053810478259_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=yfDWr-bgAXUAX_iA0Jg&_nc_ht=scontent.fdel31-1.fna&oh=00_AT8Kw68nJ__pfTvkNtq3U4_VG_hpQysuwyo1uYQLcZzbRQ&oe=623F8CEC",
-    date: "Friday, March 11, 2022",
-    title: "PYP Unit Takes First-Graders On A Voyage To Mars",
-    body: 'This year\'s Mars Home Expedition experience has been out of this world for our first-grade students! As part of their PYP unit, "Where We Are In Place and Time," the annual class project explores how we can create pr...',
+    date: "Monday, February 21, 2022",
+    title: "primary class students participating in art competition",
+    body: "",
   },
 ];
 
--- src/components/Home/SlidesData.js
+++ src/components/Home/SlidesData.js
@@ -1,67 +1,67 @@
 const slides = [
-    {
-        title: 'we create a joyful and challenging learning process',
-        img: 'https://assets.speakcdn.com/assets/2532/dsc03673.jpg',
-        class: 'd-flex flex-column flex-md-row-reverse ',
-        backgroundColor: '#597E20',
-        learnMoreLink: '/',
-        slideData: [
-            {
-                title: 'student/teacher ratio',
-                data: '7:1'
-            },
-            {
-                title: 'vertically aligned curriculum',
-                data: 'PK-12'
-            },
-            {
-                title: 'International baccalaureate diploma programme enrollment',
-                data: '45%'
-            }
-        ]
-    },
-    {
-        title: 'we forge meaningful relationships',
-        img: 'https://assets.speakcdn.com/assets/2532/20201021-dsc07922.jpg',
-        class: 'd-flex flex-column flex-md-row ',
-        backgroundColor: '#5A2049',
-        learnMoreLink: '/',
-        slideData: [
-            {
-                title: 'average class size',
-                data: '14'
-            },
-            {
-                title: 'lakeside acres',
-                data: '30'
-            },
-            {
-                title: 'siblings',
-                data: '50%'
-            }
-        ]
-    },
-    {
-        title: 'we encourage continual self-reflection',
-        img: 'https://assets.speakcdn.com/assets/2532/dsc03474.jpg',
-        class: 'd-flex flex-column flex-md-row-reverse ',
-        backgroundColor: '#D9002A',
-        learnMoreLink: '/',
-        slideData: [
-            {
-                title: 'advisory groups',
-                data: '62'
-            },
-            {
-                title: 'leaning specialists',
-                data: '4'
-            },
-            {
-                title: 'counselors',
-                data: '4'
-            }
-        ]
-    }
-]
+  {
+    title: "we create a joyful and challenging learning process",
+    img: "https://assets.speakcdn.com/assets/2532/dsc03673.jpg",
+    class: "d-flex flex-column flex-md-row-reverse ",
+    backgroundColor: "#597E20",
+    learnMoreLink: "/",
+    slideData: [
+      {
+        title: "student/teacher ratio",
+        data: "7:1",
+      },
+      {
+        title: "vertically aligned curriculum",
+        data: "PK-12",
+      },
+      {
+        title: "International baccalaureate diploma programme enrollment",
+        data: "45%",
+      },
+    ],
+  },
+  {
+    title: "we forge meaningful relationships",
+    img: "https://assets.speakcdn.com/assets/2532/20201021-dsc07922.jpg",
+    class: "d-flex flex-column flex-md-row ",
+    backgroundColor: "#5A2049",
+    learnMoreLink: "/",
+    slideData: [
+      {
+        title: "average class size",
+        data: "14",
+      },
+      {
+        title: "lakeside acres",
+        data: "30",
+      },
+      {
+        title: "siblings",
+        data: "50%",
+      },
+    ],
+  },
+  {
+    title: "unique offerings from pantheon",
+    img: "https://assets.speakcdn.com/assets/2532/dsc03474.jpg",
+    class: "d-flex flex-column flex-md-row-reverse ",
+    backgroundColor: "#D9002A",
+    learnMoreLink: "/",
+    slideData: [
+      {
+        title: "spoken english classes",
+        data: "62",
+      },
+      {
+        title: "weekly tests",
+        data: "4",
+      },
+      {
+        title: "hours daily tution",
+        data: "2",
+      },
+    ],
+  },
+];
 
-export default slides
+export default slides;
