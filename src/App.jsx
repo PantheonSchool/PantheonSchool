8d613905efed9eb3fd7273d6681983eac7c72de8
abhinavginti <abhinavginti@gmail.com>
2022-03-29 11:42:03 +0530

    Page under construction added

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Footer from "./components/Footer/Footer"
 const Navbar = lazy(() => import('./components/Navbar/Navbar'))
 const Home = lazy(() => import('./components/Home/Home'))
 
+const Page404 = lazy(() => import('./components/Page404'))
+
 const App = () => {
   return (
     <>
-      <div className='w-100' style={{minHeight:'100vh'}}>
+      <div className='w-100' style={{ minHeight: '100vh' }}>
         <Suspense fallback={<>Loading...</>}><Navbar /></Suspense>
         <Suspense fallback={<>Loading ...</>} >
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='*' element={<Page404 />} />
           </Routes>
         </Suspense>
         <Footer />
