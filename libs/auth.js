42e6ba4c2506264d4e20a583bc5337216c7c54cc
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-07-29 18:53:34 +0530

    changes

--- libs/auth.js
+++ libs/auth.js
@@ -4,13 +4,11 @@ const Admin = require("../models/admin.model");
 require("dotenv").config({ path: "../config.env" });
 
 module.exports = async (req, res, next) => {
- 
-
   try {
-     let decodedToken = jwt.verify(
-    req.headers["x-access-token"],
-    process.env.SECRET
-  );
+    let decodedToken = jwt.verify(
+      req.headers["x-access-token"],
+      process.env.SECRET
+    );
     let admin = await Admin.findOne({ username: decodedToken.username });
     if (admin) {
       res.locals.admin = admin;
