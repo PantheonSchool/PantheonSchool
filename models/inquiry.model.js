e77e82a6db0839003aa54981b8e5563685897d4d
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-07-29 18:51:08 +0530

    delete api

--- models/inquiry.model.js
+++ models/inquiry.model.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const inquiry = new mongoose.Schema({
-  fullname: { type: String, required: true },
-  contact_no: { type: String, required: true },
-  email: { type: String },
-  message: { type: String },
-});
+const inquiry = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true },
+    contact_no: { type: String, required: true },
+    email: { type: String },
+    message: { type: String },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("inquiries", inquiry);
