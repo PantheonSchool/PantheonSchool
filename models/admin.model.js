675b0ea1441237ba46e55e5cf755c074d7c67c8b
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-07-31 19:55:48 +0530

    method changes in admin login

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "../config.env" });
 
 const adminSchema = new mongoose.Schema(
   {
@@ -9,6 +11,32 @@ const adminSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Match credentials of admin
+adminSchema.statics.findByCredentials = async (username, password) => {
+  let admin = await Admin.findOne({ username: username });
+
+  if (!admin) {
+    throw new Error("User not found");
+  }
+
+  let isMatch = await bcrypt.compare(password, admin.password);
+
+  if (!isMatch) {
+    throw new Error("Incorrect Password");
+  }
+
+  return admin;
+};
+
+// Generate auth token for admin
+adminSchema.methods.generateAuthToken = async function () {
+  let admin = this;
+  let token = jwt.sign({ username: admin.username }, process.env.SECRET);
+
+  return token;
+};
+
+// Hash the plain text password before saving
 adminSchema.pre("save", async function (next) {
   const admin = this;
 
--- routers/admin.js
+++ routers/admin.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = new express.Router();
 const Admin = require("../models/admin.model");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 const auth = require("../libs/auth");
 
 require("dotenv").config({ path: "../config.env " });
@@ -19,25 +17,20 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-  let admin = await Admin.findOne({ username: req.body.username });
-  if (!admin) {
-    return res.status(401).send({ status: false, error: "username not found" });
-  }
-
-  let isPassCorrect = await bcrypt.compare(req.body.password, admin.password);
-  if (isPassCorrect) {
-    const token = await jwt.sign(
-      {
-        username: admin.username,
-      },
-      process.env.SECRET
+  try {
+    let admin = await Admin.findByCredentials(
+      req.body.username,
+      req.body.password
     );
 
-    return res
+    let token = await admin.generateAuthToken();
+
+    res
       .status(200)
       .send({ status: true, username: admin.username, token: token });
-  } else {
-    return res.status(401).send({ status: false, error: "Wrong Password" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ status: false, error: err });
   }
 });
 
