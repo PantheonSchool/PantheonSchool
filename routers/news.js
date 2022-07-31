cf855937be737fbdfc0a040410ab556bb4353bab
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-07-31 22:00:59 +0530

    news endpoint added

---
+++ routers/news.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const News = require("../models/news.model");
+const auth = require("../libs/auth");
+
+router.post("/", auth, async (req, res) => {
+  let newsData = new News(req.body);
+
+  try {
+    await newsData.save();
+    res.status(200).send({ status: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ status: false, error: err });
+  }
+});
+
+router.patch("/:id", auth, async (req, res) => {
+  try {
+    await News.findByIdAndUpdate(req.params.id, req.body);
+    res.status(201).send({ status: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ status: false, error: err });
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    await News.findByIdAndDelete(req.params.id);
+    res.send({ status: true });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ status: false, error: err });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    if (req.query.homepage) {
+      let limitNewsData = await News.find({}).sort({ date: -1 }).limit(4);
+      return res.status(200).send({ status: true, data: limitNewsData });
+    }
+
+    let newsData = await News.find({});
+    res.status(200).send({ status: true, data: newsData });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ status: false, error: err });
+  }
+});
+
+module.exports = router;
