38ef088399667052c992309c03dc2c218301f7ee
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-08-11 14:39:22 +0530

    patch2 apply

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -7,7 +7,7 @@ const Apply = () => {
     return (
         <>
             <div className="position-relative">
-                <div className="pantheon-cover cover-mod fixed d-flex flex-column justify-content-center align-items-center" style={{ background: `url(${cdnURL}/Images/news-pics/Holi.jpg)` }} >
+                <div className="pantheon-cover cover-mod fixed d-flex flex-column justify-content-center align-items-center" style={{ background: `url(${cdnURL}/Images/applyCover.jpg)` }} >
                     <p className='display-4 text-center my-auto text-uppercase hero-content fw-bold text-light'><span></span></p>
                 </div>
             </div>
