edce449e3fedcba20bbe6bddebb908fda55797c7
abhinavginti <abhinavginti@gmail.com>
2022-08-12 17:18:01 +0530

    minor banner changes

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -1,6 +1,7 @@
 import AdmissionOverview from './AdmissionOverview'
 import Instructions from './Instructions'
 import Cover from '../Cover'
+import { cdnURL } from '../../utils/constants'
 // import Banner from '../Images/applyCover.jpg'
 
 import './apply.css'
@@ -8,12 +9,12 @@ import './apply.css'
 const Apply = () => {
     return (
         <>
-            {/* <div className="position-relative">
-                <div className="pantheon-cover cover-mod fixed d-flex flex-column justify-content-center align-items-center" style={{ background: `url(${cdnURL}/Images/applyCove.jpg)` }} >
+            <div className="position-relative">
+                <div className="pantheon-cover cover-mod fixed d-flex flex-column justify-content-center align-items-center" style={{ background: `url(${cdnURL}/Images/applyCover.jpg)` }} >
                     <p className='display-4 text-center my-auto text-uppercase hero-content fw-bold text-light'><span></span></p>
                 </div>
-            </div> */}
-            <Cover backgroundImg='/Images/applyCover.jpg'/>
+            </div>
+            {/* <Cover backgroundImg='/Images/applyCover.jpg'/> */}
             <div className='container'>
                 <AdmissionOverview />
             </div>
