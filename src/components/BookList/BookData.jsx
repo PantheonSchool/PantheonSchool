9d5dd3e5ce4246e1ef580144be5316b5b475e613
abhinavginti <abhinavginti@gmail.com>
2022-08-19 20:02:46 +0530

    lkg booklist data updated

--- src/components/BookList/BookData.jsx
+++ src/components/BookList/BookData.jsx
@@ -88,6 +88,92 @@ const BookData = {
         stationary: [
             'Crayons', 'Pencil', 'Erasor and Sharpener', 'Cover and Name Slips'
         ]
+    },
+    LKG: {
+        bookList: [
+            {
+                subject: 'Math',
+                names: [{
+                    name: 'Number 1 to 100',
+                    publication: 'Book Magic'
+                }]
+            },
+            {
+                subject: 'English',
+                names: [{
+                    name: 'English Pre Primer',
+                    publication: 'Book Magic'
+                },
+                {
+                    name: 'Cursive Cap & Small',
+                    publication: 'Book Magic'
+                }]
+            },
+            {
+                subject: 'Drawing',
+                names: [{
+                    name: 'Magic Wand B',
+                    publication: 'Book Magic'
+                }]
+            },
+            {
+                subject: 'English Rhymes',
+                names: [{
+                    name: 'Nursery Rhymes B',
+                    publication: 'Book Magic'
+                }]
+            },
+            {
+                subject: 'Hindi Rhymes',
+                names: [{
+                    name: 'Baal Geet 2',
+                    publication: 'Book Magic'
+                }]
+            },
+            {
+                subject: 'Hindi',
+                names: [{
+                    name: 'Shabd Gyan',
+                    publication: 'Book Magic'
+                },
+                {
+                    name: 'Shabd Lekhan',
+                    publication: 'Book Magic'
+                }]
+            },
+            {
+                subject: 'EVS',
+                names: [{
+                    name: 'Picture Dictionary',
+                    publication: 'Book Magic'
+                }]
+            }
+        ],
+        notebookList: [
+            {
+                name: 'Hindi 5 Lines',
+                qty: 2
+            },
+            {
+                name: 'English 4 Lines',
+                qty: 2
+            },
+            {
+                name: 'Maths Square Blocks',
+                qty: 2
+            },
+            {
+                name: 'All in one',
+                qty: 1
+            },
+            {
+                name: 'Drawing Copy',
+                qty: 1
+            }
+        ],
+        stationary: [
+            'Crayons', 'Pencil', 'Erasor and Sharpener', 'Cover and Name Slips'
+        ]
     }
 }
 
