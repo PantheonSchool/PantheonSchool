51e59eed2fa7d18c5b05e08853670445b263a3e2
abhinavginti <abhinavginti@gmail.com>
2022-08-21 16:36:24 +0530

    added sitemap.xml

---
+++ src/sitemap-routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+export default (
+    <Route>
+        <Route path='/' />
+        <Route path="/inquire" />
+        <Route path='/apply' />
+        <Route path='/admission-information' />
+        <Route path='/fee-structure' />
+        <Route path='/pantheon-at-a-glance' />
+        <Route path='/director-message' />
+        <Route path='/chairman-message' />
+        <Route path='/principal-message' />
+        <Route path='/book-list' />
+        <Route path='/login' />
+        <Route path='/visit' />
+        <Route path='/news' />
+        <Route path='/contact-us' />
+    </Route>
+);
