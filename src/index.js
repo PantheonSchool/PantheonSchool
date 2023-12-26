8ca7d3549acd2b9ea94bd76598770bf06914c705
Ujjwal Pandey <76474519+Uj-08@users.noreply.github.com>
2023-12-26 19:02:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,26 +18,26 @@ const APP = (
   <AuthProvider>
     <BrowserRouter>
       <App />
-      <h1>404</h1>
     </BrowserRouter>
   </AuthProvider>)
 
-// if (rootElement.hasChildNodes()) {
-//    hydrate(APP, rootElement);
-// } else {
-//    render(APP, rootElement);
-// }
+if (rootElement.hasChildNodes()) {
+   hydrate(APP, rootElement);
+} else {
+   render(APP, rootElement);
+}
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <AuthProvider>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </AuthProvider>
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+    <h1>404</h1>
+      // <BrowserRouter>
+        // <App />
+      // </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
