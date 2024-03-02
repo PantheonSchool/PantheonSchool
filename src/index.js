8649f9caff6058f5aed33edc85fc898c7939150b
Ujjwal Pandey <76474519+Uj-08@users.noreply.github.com>
2024-03-02 14:43:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
