ac6be03f2a475df4e7590152d2c3e4de7a76cf93
Ujjwal Pandey <ujjwal.pandey@lenskart.in>
2024-08-08 21:59:11 +0530

    fixes

--- routers/review.js
+++ routers/review.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
-const apiURL = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ3QOb4CSboDkRaFoN_O3uS7I&key=AIzaSyAfM7TmNIQ6cKBvS6yswKpVzfyE0eo99pg';
 
 router.get("/", async (req, res) => {
+  const apiURL = `https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ3QOb4CSboDkRaFoN_O3uS7I&key=${process.env.GOOGLE_API_KEY}`;
   try{
       const apiData = await axios.get(apiURL);
       const reviews = apiData.data.result.reviews;
